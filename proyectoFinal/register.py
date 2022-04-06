055db08129555a8b96eced54c028173d15091494
lawMS <50967742+lawMS@users.noreply.github.com>
2022-04-06 01:43:24 -0500

    big

--- proyectoFinal/register.py
+++ proyectoFinal/register.py
@@ -69,6 +69,40 @@ class Memory:
     def __len__ (self):
         return len(self.byte)
 
+if __name__ == "__main__":
+    # 1000 bytes memory
+    memory = Memory(1000) 
+ 
+    # 16x 128 bits SSE registers
+    xmm = []
+    for i in range(16):
+        xmm.append(Register())
+ 
+    #test register aliasing    
+    xmm[0].unsigned32[:] = (4, 5, 6, 7) # fill the whole register (4 x 32 bits)
+    temp = xmm[0].float64[:] # copy as float64 (2 x 64 bits)
+    xmm[0].float64[:] = temp # write back
+    print("xmm[0]:", xmm[0].unsigned32) # (4, 5, 6, 7)
+ 
+    # memory aliasing
+    memory.unsigned32[100] = 0x882233FF
+    
+    for i in range(103, 99, -1): #Little endian !
+        print(hex(memory.byte[i]), end=" ")
+    else:
+        print()
+ 
+    # register to register
+    xmm[1].unsigned32[:] = xmm[0].unsigned32[:]
+    xmm[1].float64[0] = xmm[0].float32[3]
+ 
+    # mem to register
+    xmm[0].unsigned32[0] = memory.unsigned32[100]
+    print(hex(xmm[0].unsigned32[0])) # 0x882233FF
+
+
+
+
 class cpu:
     # Dic opCode
     #hexadecimal
@@ -118,7 +152,7 @@ hlt = False
 # Memoria de acceso aleatorio (RAM, random-access memory)
 #tam 16×8 (16 posiciones de 8 bits cada una)
 
-MemoryRam = Memory = []
+memoryRam = Memory = []
 
 
 def instrucion(opcode, register):
@@ -210,7 +244,7 @@ def instrucion(opcode, register):
         
     elif opcode == '20':
         print('CLEAR')
-        MemoryRam.clear()
+        memoryRam.clear()
     else:
         print("opcode no inscrito")
     
@@ -244,33 +278,3 @@ def verifica(numBotton, lines):
     
 
 
-if __name__ == "__main__":
-    # 1000 bytes memory
-    memory = Memory(1000) 
- 
-    # 16x 128 bits SSE registers
-    xmm = []
-    for i in range(16):
-        xmm.append(Register())
- 
-    #test register aliasing    
-    xmm[0].unsigned32[:] = (4, 5, 6, 7) # fill the whole register (4 x 32 bits)
-    temp = xmm[0].float64[:] # copy as float64 (2 x 64 bits)
-    xmm[0].float64[:] = temp # write back
-    print("xmm[0]:", xmm[0].unsigned32) # (4, 5, 6, 7)
- 
-    # memory aliasing
-    memory.unsigned32[100] = 0x882233FF
-    
-    for i in range(103, 1, 99): #Little endian !
-        print(hex(memory.byte[i]), end=" ")
-    else:
-        print()
- 
-    # register to register
-    xmm[1].unsigned32[:] = xmm[0].unsigned32[:]
-    xmm[1].float64[0] = xmm[0].float32[3]
- 
-    # mem to register
-    xmm[0].unsigned32[0] = memory.unsigned32[100]
-    print(hex(xmm[0].unsigned32[0])) # 0x882233FF
