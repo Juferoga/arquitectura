a03f3a14ef69895bdef481216f245f3a5ceda7e8
juferoga <juancholoco2000.61@gmail.com>
2022-04-06 01:04:44 -0500

    GUI functionality

--- proyectoFinal/vista.py
+++ proyectoFinal/vista.py
@@ -11,6 +11,11 @@ class Ui(QtWidgets.QMainWindow):
         self.btn_play.clicked.connect(self.startCPU)
         self.btn_debug.clicked.connect(self.startDebug)
         self.btn_load.clicked.connect(self.uploadFile)
+        
+        color="green"
+        self.lbl_status.setText("<font color="+color+">■</font>")
+
+        #self.btn_test.clicked.connect()
 
     def resetCPU(self):
         print("funciono")
@@ -25,19 +30,6 @@ class Ui(QtWidgets.QMainWindow):
         cont = contenido.split('\n')
         print(cont)
         # Envio a metodo de procesado
-
-    def setA(self):
-        self.lbl_a.setText("valor")
-    def setB(self):
-        self.lbl_b.setText("valor")
-    def setMar(self):
-        self.lbl_mar.setText("valor")
-    def setPC(self):
-        self.lbl_pc.setText("valor")
-    def setIR(self):
-        self.lbl_ir.setText("valor")
-    def setStatus(self):
-        self.lbl_status.setText("valor")
 
 def vista() :
     app = QtWidgets.QApplication(sys.argv)
